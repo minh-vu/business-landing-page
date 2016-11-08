71f3664c6f5a23f0fe326583b7b242bf707ef9a1
Vu Nguyen <Vu Nguyen>
2016-11-08 14:36:51 +0700

    update

--- assets/setting.js
+++ assets/setting.js
@@ -1,12 +1,11 @@
 // ============= HOME ==============
 var Page = {
     //-- de trong "" de giu nguyen default text
-    logoText: "Digitalife ",
-    subHeading: "Website để thể hiện. <strong>Landing page</strong> để thu hút khách hàng",
-    mainHeading: "Start <b>Online Marketing.</b> Earn <b>New Customers</b>",
-    CTA: "Tôi muốn mở kênh Online ngay",
-    brief: "Chúng tôi nhận ra rằng bạn đã có một website tuyệt vời, <br>chỉ cần thêm 1 chút bổ sung, bạn sẽ thấy hiệu quả marketing rõ rệt.",
-
+    logoText: "",
+    subHeading: "",
+    mainHeading: "",
+    CTA: "",
+    brief: "",
     // ============= VARS ==============
     url : "http://www.digitalife.top/", //-- co slash cuoi /
     mainkeyWord : "mở kênh online marketing",
@@ -14,6 +13,7 @@ var Page = {
     facebookAppId : "303370230021409", //-- your appID
     googleAnalyticId : "UA-82197343-2", //-- your id
     formId: "https://docs.google.com/forms/d/e/1FAIpQLScjBXL-M0eRB0qhX1xSV5sWWAwsXbNqtjAGm3aq0-l5EZAisg/viewform?embedded=true",
+    // formId: ""
 }
 
 var Social = {
@@ -30,9 +30,11 @@ var Social = {
 var colors = [
     "red","blue","green","orange",  // 0 1 2 3  
     "pink", "purple", "light-blue", "cyan", // 4 5 6 7  
-    "deep-purple", "indigo", "teal", "lime", // 8 9 10 11
-    "light-green", "yellow", "amber", "deep-orange", // 12 13 14 15
-    "brown", "grey", "blue-grey"      // 16 17 18 19
+    // "deep-purple", "indigo", "teal", "lime", // 8 9 10 11
+    "deep-purple", "indigo", "teal", // 8 9 10 11
+    // "light-green", "yellow", "amber", "deep-orange", // 12 13 14 15
+    "light-green", "yellow", "deep-orange", // 12 13 14 15
+    // "brown", "grey", "blue-grey"      // 16 17 18 19
 ];
 
 // colors = ['green', 'deep-purple']
